6b66e18834b8ecc91928a3961977c2ec1203f42a
momenurmaruf2742 <momenur.maruf@gmail.com>
2023-12-29 22:41:02 +0600

    gitignore and elementswap by positions

---
+++ Extra_practice_lists/swap_ele_by_position.py
@@ -0,0 +1,31 @@
+"""
+Input : List = [25, 65, 21, 90], pos1 = 1, pos2 = 3
+Output : [21, 65, 25, 90]
+
+Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
+Output : [1, 5, 3, 4, 2]
+
+
+
+"""
+
+class SwapElemetByPositios():
+    #approch1 (comma assignment)
+    """
+    Time Complexity: O(1), for using constant operations.
+    Auxiliary Space: O(1), for using constant extra space.
+    """
+    def swapElement(p_list,pos1,pos2):
+        # p_list=[]
+        p_list[pos1-1],p_list[pos2-1]=p_list[pos2-1],p_list[pos1-1]
+        return p_list
+    
+    #approch2 (temp variable)
+    """
+    Time Complexity: O(1), for using constant operations.
+    Auxiliary Space: O(1), for using constant extra space.
+    """
+    def swapElement(p_list,pos1,pos2):
+        
+calling=SwapElemetByPositios
+print(calling.swapElement([25, 65, 21, 90], pos1 = 1, pos2 = 3))
